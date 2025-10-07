974fefd117068f510791f9c8fe977d082545d5b8
kpilszak <klaudia.pilszak@gmail.com>
2025-10-07 05:44:51 -0400

    Add history list elements

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>BLUGH</li>
+        </ul>
         <nav>
           <p>Made by Klaudia</p>
         </nav>
