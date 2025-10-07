0eec80e906aef344d58c4531259810ee44fc7dc3
kpilszak <klaudia.pilszak@gmail.com>
2025-10-07 05:35:02 -0400

    Create side-bar structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 const App = () => {
   return (
     <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Klaudia</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
   )
 }
