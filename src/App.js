0f53764747199112e2bb3f9f9660113e96babfe6
kpilszak <klaudia.pilszak@gmail.com>
2025-10-13 06:01:17 -0400

    Add main section

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ const App = () => {
           <p>Made by Klaudia</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>KlaudiaGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Chat GPT Mar 14 Version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
