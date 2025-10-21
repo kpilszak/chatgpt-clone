9b4730837f5b4d6b06247d793df80c7de6dab989
kpilszak <klaudia.pilszak@gmail.com>
2025-10-21 09:05:17 -0400

    Add API_KEY to env

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const PORT = 8000
 const express = require('express')
-const cors = require('express')
+const cors = require('cors')
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+const API_KEY = process.env.API_KEY
 
 app.post('/completions', async (req, res) => {
     const options = {
